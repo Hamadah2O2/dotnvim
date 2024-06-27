b015de2a4100bd37de6245959292aa48681d70d1
Hamadah2O2 <muhammadrozaan@gmail.com>
2024-06-27 12:43:33 +0700

    change smart split keymap

--- lua/plugins/smart-split.lua
+++ lua/plugins/smart-split.lua
@@ -4,9 +4,9 @@ return {
 	config = function()
 		local splits = require("smart-splits")
 
-		vim.keymap.set("n", "<A-h>", function() splits.resize_left(2) end)
-		vim.keymap.set("n", "<A-j>", function() splits.resize_down(2) end)
-		vim.keymap.set("n", "<A-k>", function() splits.resize_up(2) end)
-		vim.keymap.set("n", "<A-l>", function() splits.resize_right(2) end)
+		vim.keymap.set("n", "<A-Left>", function() splits.resize_left(2) end)
+		vim.keymap.set("n", "<A-Down>", function() splits.resize_down(2) end)
+		vim.keymap.set("n", "<A-Up>", function() splits.resize_up(2) end)
+		vim.keymap.set("n", "<A-Right>", function() splits.resize_right(2) end)
 	end,
 }
