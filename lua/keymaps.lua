7bc72d47eee6297e7727bf7675c14f3707635cfc
Hamadah2O2 <muhammadrozaan@gmail.com>
2024-07-01 08:53:03 +0700

    Make soft line navigate

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -14,6 +14,19 @@ map({ "n", "i", "v" }, "<C-q>", "<esc>:q<cr>", {
 	silent = true,
 })
 
+-- TabNavigate
+map("n", "<A-[>", ":tabNext<CR>", opt1)
+map("n", "<A-]>", ":tabnext<CR>", opt1)
+map("n", "<A-S-t>", ":tabnew<CR>", opt1)
+
+-- Soft line navigate
+map("n", "<Down>", "gj", opt1)
+map("n", "<Up>", "gk", opt1)
+map("i", "<Down>", "<C-o>gj", opt1)
+map("i", "<Up>", "<C-o>gk", opt1)
+map("n", "j", "gj", opt1)
+map("n", "k", "gk", opt1)
+
 -- insert navigation
 -- map("i", "<C-k>", "<Up>", opt1)
 -- map("i", "<C-j>", "<Down>", opt1)
@@ -30,25 +43,17 @@ map({ "n", "i", "v" }, "<C-q>", "<esc>:q<cr>", {
 --  nnoremap j jzz
 --  nnoremap k kzz
 
--- " horizontal slide
+-- horizontal slide
 -- nnoremap <A-L> 5zl5l
 -- nnoremap <A-H> 5zh5h
--- " end horizontal slide
+-- end horizontal slide
 
--- " ccc key
---     nnoremap ccc :CccPick<CR>
-
--- " Vim Window Navigate
+-- Vim Window Navigate
 -- nnoremap <C-h> <C-w><C-h>
 -- nnoremap <C-l> <C-w><C-l>
 -- nnoremap <C-j> <C-w><C-j>
 -- nnoremap <C-k> <C-w><C-k>
--- " END
-
--- " TabNavigate
-map("n", "<A-[>", ":tabNext<CR>", opt1)
-map("n", "<A-]>", ":tabnext<CR>", opt1)
-map("n", "<A-S-t>", ":tabnew<CR>", opt1)
+-- END
 
 -- " ToggleTerm Manager
 -- nnoremap <silent> <Space>ft <Cmd>Telescope toggleterm_manager<CR>
