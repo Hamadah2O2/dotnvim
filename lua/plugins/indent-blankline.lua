51124f673b130d751a78dcaac9d5d8b510d5c5e2
Hamadah2O2 <muhammadrozaan@gmail.com>
2024-07-01 08:50:10 +0700

    added indent blankline and tailwind fold plugins

---
+++ lua/plugins/indent-blankline.lua
@@ -0,0 +1,14 @@
+return {
+	"lukas-reineke/indent-blankline.nvim",
+	event = {
+		"BufReadPost",
+		"BufNewFile",
+	},
+	main = "ibl",
+	opts = {},
+	config = function()
+		require("ibl").setup({
+			scope = { enabled = false },
+		})
+	end,
+}
