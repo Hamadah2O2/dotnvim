fcfb73f9260f4addf68b8ac27a1b72bea6ae5f68
Hamadah2O2 <muhammadrozaan@gmail.com>
2024-07-11 10:59:18 +0700

    Jsx stuck fix

--- lua/options.lua
+++ lua/options.lua
@@ -1,28 +1,29 @@
 local o = vim.opt
 
-o.showmatch = true -- show matching
-o.ignorecase = true -- case insensitive
-o.mouse = "v" -- middle-click paste with
-o.hlsearch = true -- highlight search
-o.incsearch = true -- incremental search
-o.tabstop = 2 -- number of columns occupied by a tab
-o.softtabstop = 2 -- see multiple spaces as tabstops so <BS> do>
-o.expandtab = true -- converts tabs to white space
-o.shiftwidth = 2 -- width for autoinden:s
-o.autoindent = true -- indent a new line the same amount as the l>
-o.number = true -- add line numbers
-o.relativenumber = true -- relative number on
-o.wildmode = "full" -- Comand Auto COmplete Listing
-o.mouse = "a" -- mouse click
+o.showmatch = true          -- show matching
+o.ignorecase = true         -- case insensitive
+o.mouse = "v"               -- middle-click paste with
+o.hlsearch = true           -- highlight search
+o.incsearch = true          -- incremental search
+o.tabstop = 2               -- number of columns occupied by a tab
+o.softtabstop = 2           -- see multiple spaces as tabstops so <BS> do>
+o.expandtab = true          -- converts tabs to white space
+o.shiftwidth = 2            -- width for autoinden:s
+o.autoindent = true         -- indent a new line the same amount as the l>
+o.smartindent = true
+o.number = true             -- add line numbers
+o.relativenumber = true     -- relative number on
+o.wildmode = "full"         -- Comand Auto COmplete Listing
+o.mouse = "a"               -- mouse click
 o.clipboard = "unnamedplus" -- systen clipboard
-o.linebreak = true -- Break Words by Words
-o.breakindent = true -- Space Indention
-o.cursorline = true -- Cursor Line
+o.linebreak = true          -- Break Words by Words
+o.breakindent = true        -- Space Indention
+o.cursorline = true         -- Cursor Line
 
-o.ttyfast = true -- speed up scrolling
-o.sidescroll = 1 -- enable side scrolling
-o.so = 999 -- cursor on center
-o.splitbelow = true -- Split on bellow
+o.ttyfast = true            -- speed up scrolling
+o.sidescroll = 1            -- enable side scrolling
+o.so = 999                  -- cursor on center
+o.splitbelow = true         -- Split on bellow
 -- o.nowrap = true
 o.foldlevel = 20
 o.foldmethod = "expr"
@@ -43,8 +44,9 @@ vim.opt.updatetime = 300
 -- diagnostics appeared/became resolved
 vim.opt.signcolumn = "yes"
 
-o.laststatus=3 -- just one status line pls
+o.laststatus = 3 -- just one status line pls
 
 vim.cmd([[filetype plugin indent on]])
 vim.cmd([[filetype plugin on]])
 vim.cmd([[autocmd FileType php setlocal autoindent]])
+vim.cmd([[autocmd FileType javascriptreact setlocal tabstop=2 softtabstop=2 shiftwidth=2 expandtab]])
