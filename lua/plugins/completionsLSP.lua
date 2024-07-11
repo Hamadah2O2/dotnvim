7fbe1c1f02f886c17840d52fa0954c214a28fa13
Hamadah2O2 <muhammadrozaan@gmail.com>
2024-07-11 10:58:25 +0700

    fix coc

--- lua/plugins/completionsLSP.lua
+++ lua/plugins/completionsLSP.lua
@@ -1,31 +1,60 @@
-local command = { "CocCommand", "CocConfig", "CocDiagnostics", "CocDisable", "CocEnable", "CocFirst", "CocInfo", "CocInstall", "CocLast", "CocList", "CocListCancel", "CocListResume", "CocLocalConfig", "CocNext", "CocOpenLog", "CocOutline", "CocPrev", "CocPrintErrors", "CocRestart", "CocSearch", "CocStart", "CocUninstall", "CocUpdate", "CocUpdateSync", "CocWatch", }
+local command = {
+  "CocCommand",
+  "CocConfig",
+  "CocDiagnostics",
+  "CocDisable",
+  "CocEnable",
+  "CocFirst",
+  "CocInfo",
+  "CocInstall",
+  "CocLast",
+  "CocList",
+  "CocListCancel",
+  "CocListResume",
+  "CocLocalConfig",
+  "CocNext",
+  "CocOpenLog",
+  "CocOutline",
+  "CocPrev",
+  "CocPrintErrors",
+  "CocRestart",
+  "CocSearch",
+  "CocStart",
+  "CocUninstall",
+  "CocUpdate",
+  "CocUpdateSync",
+  "CocWatch",
+}
 
 return {
-	-- { "mattn/emmet-vim" },
-	{
-		"neoclide/coc.nvim",
-		branch = "release",
-		lazy = true,
-		event = {
-			"InsertEnter",
-			-- "VeryLazy",
-		},
-		cmd = command,
-		dependencies = {
-			{ "windwp/nvim-autopairs" },
-			{ "SirVer/ultisnips" },
-			{ "honza/vim-snippets" },
-		},
-		config = function()
-			require("configs.coc")
-		end,
-	},
+  -- { "mattn/emmet-vim" },
+  {
+    "neoclide/coc.nvim",
+    branch = "release",
+    lazy = true,
+    event = {
+      "InsertEnter",
+      -- "VeryLazy",
+    },
+    cmd = command,
+    dependencies = {
+      { "windwp/nvim-autopairs" },
+      { "SirVer/ultisnips" },
+      { "honza/vim-snippets" },
+      { "neoclide/vim-jsx-improve" },
+      { "maxmellon/vim-jsx-pretty" }
+    },
+    config = function()
+      require("configs.coc")
+    end,
+  },
 
-	{
-		"windwp/nvim-autopairs",
-		event = "InsertEnter",
-		config = true,
-		-- use opts = {} for passing setup options
-		-- this is equalent to setup({}) function
-	},
+  {
+    "windwp/nvim-autopairs",
+    -- enabled = false,
+    event = "InsertEnter",
+    config = true,
+    -- use opts = {} for passing setup options
+    -- this is equalent to setup({}) function
+  },
 }
