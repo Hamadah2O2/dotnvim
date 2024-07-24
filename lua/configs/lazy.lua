8c324a4c7a47299fbaba15234504324c185bdc60
Hamadah2O2 <muhammadrozaan@gmail.com>
2024-07-24 14:05:10 +0700

    Update Lazy config

--- lua/configs/lazy.lua
+++ lua/configs/lazy.lua
@@ -1,32 +1,44 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
 local lazyConfig = {
-	default = { lazy = true },
-	install = { colorscheme = { "catppuccin" } },
-	git = {
-		log = { "-5" }, -- show the last 5 commits
-		timeout = 120, -- kill processes that take more than 2 minutes
-		url_format = "https://github.com/%s.git",
-		-- lazy.nvim requires git >=2.19.0. If you really want to use lazy with an older version,
-		-- then set the below to false. This should work, but is NOT supported and will
-		-- increase downloads a lot.
-		filter = true,
-  change_detection = false,
-	},
+  default = { lazy = true },
+  install = { colorscheme = { "catppuccin" } },
+  git = {
+    log = { "-5" }, -- show the last 5 commits
+    timeout = 120,  -- kill processes that take more than 2 minutes
+    url_format = "https://github.com/%s.git",
+    -- lazy.nvim requires git >=2.19.0. If you really want to use lazy with an older version,
+    -- then set the below to false. This should work, but is NOT supported and will
+    -- increase downloads a lot.
+    filter = true,
+  },
+  checker = {
+    -- automatically check for plugin updates
+    enabled = false,
+    concurrency = nil, ---@type number? set to 1 to check for updates very slowly
+    notify = true,        -- get a notification when new updates are found
+    frequency = 3600,     -- check for updates every hour
+    check_pinned = false, -- check for pinned packages that can't be updated
+  },
+  change_detection = {
+    -- automatically check for config file changes and reload the ui
+    enabled = true,
+    notify = false, -- get a notification when changes are found
+  },
 }
 
 require("lazy").setup({
-	{ import = "plugins" },
+  { import = "plugins" },
 }, lazyConfig)
