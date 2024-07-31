a411c7bb1fd8ca190933cf06733b49f136ad5c69
Hamadah2O2 <muhammadrozaan@gmail.com>
2024-07-31 22:02:21 +0700

    treesitter update

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -7,6 +7,17 @@ return {
     "HiPhish/nvim-ts-rainbow2",
   },
   config = function()
+    local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+
+    parser_config.blade = {
+      install_info = {
+        url = "https://github.com/EmranMR/tree-sitter-blade",
+        files = { "src/parser.c" },
+        branch = "main",
+      },
+      filetype = "blade"
+    }
+
     require("nvim-treesitter.configs").setup({
       indent = {
         enable = true,
