745bc3148bd91d8abf85c744d02c8c26c29902f3
Hamadah2O2 <muhammadrozaan@gmail.com>
2024-11-20 21:14:48 +0700

    what? init.lua in my plugins? well ...

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,26 +1,26 @@
 return {
-	-- Completion
-	-- 'yaegassy/coc-intelephense', {'do': 'yarn install --frozen-lockfile'},
-	-- 'Jezda1337/nvim-html-css',
+  -- Completion
+  -- 'yaegassy/coc-intelephense', {'do': 'yarn install --frozen-lockfile'},
+  -- 'Jezda1337/nvim-html-css',
 
-	-- Helper & Tools
-	{
-		"dstein64/vim-startuptime",
-		event = "VeryLazy",
-		cmd = "StartupTime",
-	},
+  -- Helper & Tools
+  {
+    "dstein64/vim-startuptime",
+    event = "VeryLazy",
+    cmd = "StartupTime",
+  },
 
-	"NeogitOrg/neogit",
-	"mbbill/undotree",
-	-- 'smoka7/multicursors.nvim',
-	-- 'mhinz/vim-startify',
+  -- "NeogitOrg/neogit",
+  -- "mbbill/undotree",
+  -- 'smoka7/multicursors.nvim',
+  -- 'mhinz/vim-startify',
 
-	-- Colorer
-	-- "norcalli/nvim-colorizer.lua",
+  -- Colorer
+  -- "norcalli/nvim-colorizer.lua",
 
-	"tpope/vim-surround",
+  "tpope/vim-surround",
 
-	-- "mfussenegger/nvim-treehopper",
+  -- "mfussenegger/nvim-treehopper",
 
-	"stevearc/conform.nvim",
+  -- "stevearc/conform.nvim",
 }
