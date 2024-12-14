370df16be224e8eb6fe5920796cdc234f54a939c
Hamadah2O2 <muhammadrozaan@gmail.com>
2024-12-14 11:47:14 +0700

    Update cmp Snippets folder location

--- lua/configs/cmp.lua
+++ lua/configs/cmp.lua
@@ -4,8 +4,8 @@ local cmp = require'cmp'
 -- load vscode style snippets
 require("luasnip.loaders.from_vscode").lazy_load({})
 -- require("luasnip.loaders.from_vscode").lazy_load({paths = "~/.config/nvim/sbwahsnip"})
-require("luasnip.loaders.from_lua").lazy_load({paths = "~/.config/nvim/lua/snippets/"})
-require("luasnip.loaders.from_snipmate").lazy_load({paths = "~/.config/nvim/snipmate"})
+require("luasnip.loaders.from_lua").lazy_load({paths = "~/.config/nvim/snippets/lua"})
+require("luasnip.loaders.from_snipmate").lazy_load({paths = "~/.config/nvim/snippets/snipmate"})
 
 cmp.setup({
   completion = {
