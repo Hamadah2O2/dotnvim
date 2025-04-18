cc80c3438d38c184e2c02092bc1563bb3851919c
Hamadah <muhammadrozaan@gmail.com>
2025-04-18 19:23:02 +0700

    feat(plugin): disable tailwind fold

--- lua/plugins/tailwind-fold.lua
+++ lua/plugins/tailwind-fold.lua
@@ -1,6 +1,7 @@
 return {
   "razak17/tailwind-fold.nvim",
   opts = {},
+  enabled = false,
   dependencies = { "nvim-treesitter/nvim-treesitter" },
   -- ft = { "html", "svelte", "astro", "vue", "typescriptreact", "php", "blade" },
   cmd = {
