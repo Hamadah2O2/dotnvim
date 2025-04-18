e201b25c7c967dc110a990aebe8b0f9071d0c392
Hamadah <muhammadrozaan@gmail.com>
2025-04-18 19:22:27 +0700

    feat(barbar): Remove zsh exculde name

--- lua/plugins/barbar.lua
+++ lua/plugins/barbar.lua
@@ -29,7 +29,7 @@ return {
 
 			-- Excludes buffers from the tabline
 			exclude_ft = {},
-			exclude_name = { "package.json", "zsh", "/usr/bin/zsh" },
+			exclude_name = { "package.json" },
 
 			-- A buffer to this direction will be focused (if it exists) when closing the current buffer.
 			-- Valid options are 'left' (the default), 'previous', and 'right'
