e98155f0cf87fb6bb3d365253679964cb0c8ee8d
Hamadah <muhammadrozaan@gmail.com>
2025-08-04 05:59:38 +0700

    feat(lsp): Remake how lsp enable, check if the lsp is installed or not
    Need to check lsp installed in Mason later

--- lua/configs/lsp/lsplist.lua
+++ lua/configs/lsp/lsplist.lua
@@ -11,39 +11,89 @@ local capabilities = cmp_nvim_lsp.default_capabilities()
 local capabilitiesHtml = vim.lsp.protocol.make_client_capabilities()
 -- capabilities.textDocument.completion.completionItem.snippetSupport = true
 
--- Web lsp
-lspconfig.html.setup {
-  init_options = {
-    provideFormatter = true
-  },
-  capabilities = capabilities
+local servers = {
+    -- Webdev Language Servers
+    html = {
+        filetypes = { "html", "php", "blade" },
+        supersaian = true,
+        init_options = {
+            provideFormatter = true
+        },
+        capabilities = capabilities
+    },
+    eslint = {
+        autostart = false,
+        capabilities = capabilities,
+    },
+    cssls = {
+        capabilities = capabilities,
+    },
+    ts_ls = {
+        capabilities = capabilities,
+    },
+    tailwindcss = {
+        autostart = false,
+        capabilities = capabilities,
+    },
+    intelephense = {
+        capabilities = capabilities,
+    },
+    emmet_language_server = {
+        filetypes = { "astro", "blade", "css", "eruby", "html", "htmldjango", "javascriptreact", "less", "pug", "sass", "scss", "svelte", "typescriptreact", "vue", "htmlangular", "php" },
+        capabilities = capabilities,
+    },
+    -- Go Language Server
+    gopls = {
+        capabilities = capabilities
+    },
+    -- Sqls
+    sqls = {
+        capabilities = capabilities,
+    },
+    -- lua
+    lua_ls = {
+        capabilities = capabilities,
+        settings = {
+            Lua = {
+                runtime = {
+                    -- Pastikan path sudah ke runtime Neovim Lua
+                    version = 'LuaJIT',
+                    path = vim.split(package.path, ';'),
+                },
+                diagnostics = {
+                    globals = { 'vim' },
+                },
+                workspace = {
+                    library = vim.api.nvim_get_runtime_file("", true),
+                    checkThirdParty = false, -- agar tidak muncul warning
+                },
+                telemetry = { enable = false },
+            },
+        },
+    }
 }
-lspconfig["eslint"].setup({
-  autostart = false,
-  capabilities = capabilities,
-})
-lspconfig["cssls"].setup({
-  capabilities = capabilities,
-})
-lspconfig["ts_ls"].setup({
-  autostart = false,
-  capabilities = capabilities,
-})
-lspconfig["tailwindcss"].setup({
-  autostart = false,
-  capabilities = capabilities,
-})
-lspconfig['intelephense'].setup({
-  capabilities = capabilities,
-})
--- lspconfig["emmet_ls"].setup({
-lspconfig["emmet_language_server"].setup({
-  filetypes = { "astro", "css", "eruby", "html", "htmldjango", "javascriptreact", "less", "pug", "sass", "scss", "svelte", "typescriptreact", "vue", "htmlangular", "php" },
-  capabilities = capabilities,
-})
-lspconfig["gopls"].setup{
-  capabilities = capabilities
-}
-lspconfig["lemminx"].setup{
-  capabilities = capabilities
-}
+
+local function is_installed(name)
+    local custom_executables = vim.lsp.config[name].cmd[1]
+    local executable = custom_executables or name
+
+    return vim.fn.executable(executable) == 1
+end
+
+for lsp, config in pairs(servers) do
+    if is_installed(lsp) then
+        -- Default
+        if config == true then
+            vim.lsp.config[lsp] = {
+                capabilities = capabilities
+            }
+        elseif type(config) == 'table' then
+            vim.lsp.config[lsp] = config
+        end
+        if config.autostart or config.autostart == nil then
+            vim.lsp.enable(lsp)
+        end
+    else
+        vim.notify(string.format("[LSP] %s tidak terinstall", lsp), vim.log.levels.WARN)
+    end
+end
